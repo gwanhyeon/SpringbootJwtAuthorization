b8a60409398eef2f86959738b6f2771c685dc5f1
gwanhyeon <kgh940525@gmail.com>
2020-11-25 23:48:17 +0900

    Create Springboot JDK11 Project

---
+++ src/test/java/com/kgh/jwtauthorization/JwtAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgh.jwtauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
